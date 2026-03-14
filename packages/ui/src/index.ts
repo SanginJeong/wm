34885e159e1f82db0f55961b86129c60d323df53
SanginJeong <sanginjeong07@gmail.com>
2026-03-15 00:09:55 +0900

    feat: make IconLink Component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -8,3 +8,4 @@ export { default as GlobalStyles } from "./theme/GlobalStyles";
 export { default as Button } from "./components/Button";
 export { default as Typography } from "./components/Typography";
 export { default as Input } from "./components/Input";
+export { default as IconLink } from "./components/IconLink/IconLink";
