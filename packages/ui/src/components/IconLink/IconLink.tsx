9beed3cb6e15f3f1d14292bfdd1d6a840a389d6e
SanginJeong <sanginjeong07@gmail.com>
2026-03-15 19:15:01 +0900

    fix: iconLink hover, cursor append

--- packages/ui/src/components/IconLink/IconLink.tsx
+++ packages/ui/src/components/IconLink/IconLink.tsx
@@ -14,8 +14,10 @@ const StyledIconLink = styled.a`
   padding: ${({ theme }) => theme.spacing[2]};
   border-radius: ${({ theme }) => theme.radii.full};
   transition: background-color ${({ theme }) => theme.transitions.base};
+  cursor: pointer;
 
   &:hover {
+    color: ${({ theme }) => theme.colors.white};
     background-color: ${({ theme }) => theme.colors.primary[300]};
   }
 `;
